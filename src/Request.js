f290406ec84e5daadbed054d4144b76f63942e22
victor <victorezeabikwa@gmail.com>
2024-04-02 16:36:36 +0100

    movie app

--- src/Request.js
+++ src/Request.js
@@ -1,14 +1,12 @@
-import 'dotenv/config'
-const key = process.env.REACT_APP_KEY
 
-console.log(key, 'request key')
+console.log(process.env.REACT_APP_KEY, 'request key')
 
 const requests = {
-    requestPopular:`http://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en=US&page=1`,
-    requestTopRated: `http://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    requestTrending: `http://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-    requestHorror: `http://api.themoviedb.org/3/movie?api_key=${key}&language=en-US&querry=horror&page=1`,
-    requestUpcoming: `http://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestPopular:`http://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_KEY}&language=en=US&page=1`,
+    requestTopRated: `http://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_KEY}&language=en-US&page=1`,
+    requestTrending: `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_KEY}&language=en-US&page=2`,
+    requestHorror: `http://api.themoviedb.org/3/movie?api_key=${process.env.REACT_APP_KEY}&language=en-US&querry=horror&page=1`,
+    requestUpcoming: `http://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_KEY}&language=en-US&page=1`,
 
 
 
